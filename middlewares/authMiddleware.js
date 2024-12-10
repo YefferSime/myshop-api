887648c9ea75ee9baccdc28b427a38336a6c2555
YefferSime <yeffer.sime@tecsup.edu.pe>
2024-12-10 18:48:46 -0500

    deploy backend v7

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,17 +1,22 @@
 const jwt = require('jsonwebtoken');
 
 module.exports.authMiddleware = async (req, res, next) => {
-    const { accessToken } = req.cookies
-    if (!accessToken) {
-        return res.status(409).json({ error: 'Please login first' })
-    } else {
-        try {
-            const deCodeToken = await jwt.verify(accessToken, process.env.SECRET)
-            req.role = deCodeToken.role
-            req.id = deCodeToken.id
-            next()
-        } catch (error) {
-            return res.status(409).json({ error: 'Please login' })
+    const { authorization } = req.headers
+    if (authorization) {
+        const token = authorization.split('Bearer')[1]
+        if (!token) {
+            return res.status(409).json({ error: 'Please login first' })
+        } else {
+            try {
+                const deCodeToken = await jwt.verify(token, process.env.SECRET)
+                req.role = deCodeToken.role
+                req.id = deCodeToken.id
+                next()
+            } catch (error) {
+                return res.status(409).json({ error: 'Please login' })
+            }
         }
+    }else{
+        return res.status(409).json({ error: 'Please login' })
     }
 }
