e79cb76a86157ac8aadd9ff3603a226c1b1bf75a
YefferSime <yeffer.sime@tecsup.edu.pe>
2024-12-10 19:08:18 -0500

    deploy backend v8

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,22 +1,25 @@
 const jwt = require('jsonwebtoken');
 
 module.exports.authMiddleware = async (req, res, next) => {
-    const { authorization } = req.headers
-    if (authorization) {
-        const token = authorization.split('Bearer')[1]
-        if (!token) {
-            return res.status(409).json({ error: 'Please login first' })
-        } else {
-            try {
-                const deCodeToken = await jwt.verify(token, process.env.SECRET)
-                req.role = deCodeToken.role
-                req.id = deCodeToken.id
-                next()
-            } catch (error) {
-                return res.status(409).json({ error: 'Please login' })
-            }
-        }
-    }else{
-        return res.status(409).json({ error: 'Please login' })
+    const { authorization } = req.headers;
+    if (!authorization) {
+        console.log('Authorization header is missing');
+        return res.status(401).json({ error: 'Authorization header is missing' });
     }
-}
+
+    const token = authorization.split(' ')[1];
+    if (!token) {
+        console.log('Token is missing');
+        return res.status(401).json({ error: 'Token is missing. Please login first.' });
+    }
+
+    try {
+        const deCodeToken = await jwt.verify(token, process.env.SECRET);
+        req.role = deCodeToken.role;
+        req.id = deCodeToken.id;
+        next();
+    } catch (error) {
+        console.log('Invalid or expired token');
+        return res.status(401).json({ error: 'Invalid or expired token. Please login again.' });
+    }
+};
